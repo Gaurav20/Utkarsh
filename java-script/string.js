95253f5e6389c2bfc02822c1c7f1ff8adc4bf878
Gaurav <gauravkumargk9518@gmail.com>
2022-12-30 10:56:49 +0530

    first commit

---
+++ java-script/string.js
@@ -0,0 +1,142 @@
+1+2
+3
+
+'3'==='3'
+true
+
+''==='0'
+false
+
+''==0
+true
+
+'3'===3
+false
+
+true && false
+false
+
+if(true && false)
+{}
+undefined
+
+for (var x=0; x<=4; x++)
+    {}
+undefined
+
+typeof 3
+'number'
+
+typeof 'asdf'
+'string'
+
+typeof null
+'object'
+
+typeof true
+'boolean'
+
+typeof []
+'object'
+
+typeof undefined
+'undefined'
+
+typeof {}
+'object'
+
+var sym1=Symbol('foo');
+undefined
+
+typeof sym1
+'symbol'
+
+var x=10;
+x=12
+
+var obj={
+    name:"Gaurav",
+};
+
+var infinite=Infinity;
+
+var not_a_number=NaN;
+
+typeof NaN
+'number'
+
+NaN==NaN;
+false
+
+null===null;
+true
+
+null==null;
+true;
+
+undefined===undefined;
+true
+
+
+typeof NaN
+'number'
+
+NaN===NaN 
+false
+
+NaN==NaN
+false
+
+null === null
+true
+
+null==null
+true
+
+undefined===undefined
+true
+
+isNaN(NaN)
+true
+
+isNaN(2)
+false
+
+isFinite(2)
+true
+
+
+isFinite(Infinity)
+false
+
+var infinite=Infinity;
+
+var not_a_number=NaN;
+
+typeof NaN;
+"Number"
+
+NaN==NaN;
+false
+
+NaN===NaN;
+false
+
+undefined===undefined;
+true
+
+var name="Gaurav"
+
+console.log(name);
+
+var x=10;
+var y=20;
+sum=x+y;
+console.log(sum)
+
+
+
+
+
+
+
